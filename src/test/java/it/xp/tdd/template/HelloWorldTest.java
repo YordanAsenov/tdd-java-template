766ebb6627be36bc65d9c1b108240f2a7b7f160f
Yordan Asenov <yordan.asenov.ext@siemens.com>
2023-11-04 23:56:08 +0100

    :art: clean structure for the first ever test

---
+++ src/test/java/it/xp/tdd/template/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package it.xp.tdd.template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class HelloWorldTest {
+
+    @Test
+    void whateverItDoes() {
+        fail();
+    }
+}
